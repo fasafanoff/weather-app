6acf19343ce31e5ff51f76e31caf4754d85c9afc
fasafan <rlender213@gmail.com>
2021-01-10 14:36:56 +0200

    Added hour cards

--- src/classes/HourCards.js
+++ src/classes/HourCards.js
@@ -1,5 +1,17 @@
+import HourCard from './HourCard';
+import "../css/HourCards.css"
+
 function HourCards(props) {
-  return <div></div>;
+
+    let hours = [];
+    for (let i = 0; i < 8; ++i)//we'll use 8 hour points separeted by 3 hours
+    {
+        // for now temperature is random
+        hours.push(<HourCard time={`${i * 3}:00`} temp={Math.floor(Math.random() * 10)} key={i/**indecies is safe to use */}/>)
+    }
+
+
+    return <div className="hour-cards">{hours}</div>;
 }
 
 export default HourCards;
