8bae42192639485c99a95ee96d55d52ce86a8cb2
fasafan <rlender213@gmail.com>
2021-01-10 13:50:10 +0200

    Added days strip to the weather app

--- src/classes/DayCard.js
+++ src/classes/DayCard.js
@@ -1,6 +1,35 @@
+
+import "./../css/DayCard.css"
+
+
+//Object that map weather(ex. cloudy)and link to an img
+const map = {
+  "cloudy": "https://duckduckgo.com/assets/weather/svg/new/cloudy.svg",
+};
+
+
+
+/**
+ * 
+ * @param {Object} props 
+ * {
+ *      onClick,
+ *      dow, - day of the week
+ *      weather,
+ *      max, 
+ *      min, --max and min temperature    
+ * }
+ */
 function DayCard(props)
 {
-    return(<div></div>)
+    return (
+      <div className="day-card" onClick={props.onClick}>
+        <div>{props.dow}</div>
+            <img alt={props.weather }src={map[props.weather]}/>
+        <span>{props.max}</span>
+        <span>{props.min}</span>
+      </div>
+    );
 }
 
 export default DayCard;
--- src/classes/DayCards.js
+++ src/classes/DayCards.js
@@ -1,5 +1,22 @@
+import DayCard from "./DayCard.js";
+import "./../css/DayCards.css"
+
+//days of the week
+const dow = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+
 function DayCards(props) {
-  return <div></div>;
+
+    var Days = [];
+    for (let i = 0; i < 8; ++i)//we'll display 8 days from now on
+    {
+        //for now we assume that the information is static, later we'll keep that infomation up-to-date
+        Days.push(<DayCard dow={dow[i % 7]} max={"10"} min={"-10"} key={i /**I think it's save to use indecies*/} weather={"cloudy" }/>);
+    }
+
+
+
+    return <div className="day-cards">{Days}</div>;
 }
 
 export default DayCards;
